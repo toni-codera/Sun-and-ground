538514ae51292b54d14dff0f587cb203dc328edc
toni_codera <anton.todorov99@yahoo.com>
2025-08-27 21:10:50 +0300

    Products database created

--- db.sql
+++ db.sql
@@ -8,5 +8,26 @@ CREATE TABLE users(
     city VARCHAR(25) NOT NULL,
     home_address VARCHAR(255) NOT NULL,
     phone VARCHAR(20) NOT NULL,
-    created_at DATETIME NOT NULL DEFAULT CURRENT_TIME
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIME,
+    is_admin TINYINT(1) NOT NULL DEFAULT 0
+);
+
+CREATE TABLE products (
+    id INT(10) PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    category VARCHAR(50),
+    options INT(10) DEFAULT 1,
+    variety VARCHAR(50),
+    image_path VARCHAR(255),
+    cultivation VARCHAR(50),
+    characteristics TEXT,
+    brief_description TEXT
+);
+
+CREATE TABLE product_variations (
+    id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    product_id INT(11) NOT NULL,
+    weight_grams INT(11) NOT NULL,
+    price_cents INT(11) NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
 );
